7295fe06fb5a5788d94faf26ebb5e76f5440abf0
Antoine Eddi <Antoine Eddi>
2016-05-01 22:59:29 +0200

    Add landing page and empty map page

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,11 +1,32 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController} from 'ionic-angular';
+import {MapPage} from '../map/map';
 
+const currentLabel = "Use current position"
+const addressLabel = "Use this address"
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
+  static get parameters() {
+    return [[NavController]]
+  }
 
+  constructor(nav) {
+    this.nav = nav;
+
+    this.searchQuery = ''
+    this.buttonLabel = currentLabel
+  }
+
+  searchBarInput(e) {
+//    if (this.searchQuery == '')
+//      this.buttonLabel = currentLabel
+//    else
+//      this.buttonLabel = addressLabel
+  }
+
+  goToMap() {
+    this.nav.push(MapPage, {HomePage: this})
   }
 }
---
+++ app/pages/map/map.js
@@ -0,0 +1,9 @@
+import {Page} from 'ionic-angular';
+
+@Page({
+  templateUrl: 'build/pages/map/map.html'
+})
+export class MapPage {
+  constructor() {
+  }
+}
