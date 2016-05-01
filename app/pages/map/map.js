1e1c506eb3f1f0752d31c10d6e63fa434c86d4a9
Antoine Eddi <Antoine Eddi>
2016-05-01 23:40:29 +0200

    Add map

--- app/pages/map/map.js
+++ app/pages/map/map.js
@@ -1,9 +1,38 @@
-import {Page} from 'ionic-angular';
+import {Page, Geolocation} from 'ionic-angular';
 
 @Page({
   templateUrl: 'build/pages/map/map.html'
 })
 export class MapPage {
   constructor() {
+    this.map = null;
+    this.loadMap();
   }
+
+  loadMap(){
+    let options = {timeout: 10000, enableHighAccuracy: true};
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+          let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          let mapOptions = {
+              center: latLng,
+              zoom: 15,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+          }
+          this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+          let marker = new google.maps.Marker({
+            map: this.map,
+            animation: google.maps.Animation.DROP,
+            position: this.map.getCenter()
+          });
+      },
+
+      (error) => {
+          console.log(error);
+      }, options
+    );
+  }
+
 }
